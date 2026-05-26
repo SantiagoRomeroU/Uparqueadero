06185e06e1bc135db8ee4d11ccc8d870481e915a
SantiagoRomeroU <159743594+SantiagoRomeroU@users.noreply.github.com>
2026-05-26 00:51:33 -0500

    Update App.py

--- App.py
+++ App.py
@@ -172,6 +172,4 @@ def registro():
 # ─── Punto de entrada ──────────────────────────────────────────────────────────
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True)
